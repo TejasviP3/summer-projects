96925f3c1c03fd39bb9b3aa6631fd4708e34d690
Tejasvi Prattipati <tejasviprattipati@Tejasvis-MacBook-Pro.local>
2026-07-16 20:10:53 +0530

    added additional features to Study Tracker: minutes, total minutes studied, most studied and longest time, introduction note

--- study_tracker/study_tracker.py
+++ study_tracker/study_tracker.py
@@ -1,3 +1,6 @@
+print("This is a study tracker.")
+print("Input a subject and the amount of time it was studied to begin. \nTo finish entering subjects, type \"quit\".")
+
 sessions = []
 
 while True: 
@@ -6,6 +9,24 @@ while True:
     if subject == "quit":
         break
     
-    sessions.append(subject)
+    minutes = int(input("Minutes studied: "))
+    sessions.append((subject, minutes))
 
-print(sessions)
+print("List of subjects:", sessions)
+
+total_mins = 0
+
+for subject, minutes in sessions:
+    total_mins += minutes
+
+print("Total minutes studied:", total_mins, "minutes")
+
+most_studied = ""
+highest_time = 0
+
+for subject, minutes in sessions:
+    if minutes > highest_time:
+        highest_time = minutes
+        most_studied = subject
+
+print("Your most-studied subject was", most_studied, "which you studied for", highest_time, "minutes")
